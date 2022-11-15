108eb7ddea8475cf218202700ce87ded7f5d8522
wanghao <whateverhow@gmail.com>
2022-11-15 10:50:05 -0600

    [master] 	modified:   dist/utils/dateUtils.js

--- dist/utils/dateUtils.js
+++ dist/utils/dateUtils.js
@@ -42,23 +42,23 @@ function timeSince(date) {
     const seconds = Math.floor((new Date().getTime() - new Date(date).getTime()) / 1000);
     let interval = seconds / 31536000;
     if (interval > 1) {
-        return Math.floor(interval) + " years";
+        return Math.floor(interval) + ' years';
     }
     interval = seconds / 2592000;
     if (interval > 1) {
-        return Math.floor(interval) + " months";
+        return Math.floor(interval) + ' months';
     }
     interval = seconds / 86400;
     if (interval > 1) {
-        return Math.floor(interval) + " days";
+        return Math.floor(interval) + ' days';
     }
     interval = seconds / 3600;
     if (interval > 1) {
-        return Math.floor(interval) + " hours";
+        return Math.floor(interval) + ' hours';
     }
     interval = seconds / 60;
     if (interval > 1) {
-        return Math.floor(interval) + " minutes";
+        return Math.floor(interval) + ' minutes';
     }
-    return Math.floor(seconds) + " seconds";
+    return Math.floor(seconds) + ' seconds';
 }
