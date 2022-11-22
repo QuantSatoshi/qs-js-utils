c9342b69970456051ecd5c48dfe517003b48df97
wanghao <whateverhow@gmail.com>
2022-11-21 23:50:18 -0600

    [master] 	modified:   dist/utils/commonUtils.d.ts

--- dist/utils/commonUtils.d.ts
+++ dist/utils/commonUtils.d.ts
@@ -10,3 +10,4 @@ export declare function getMemoryUsage(): number;
 export declare function sha1Hash(data: string): string;
 export declare function handleException(): void;
 export declare function generateReverseMap(mapObj: Record<string, string>): Record<string, string>;
+export declare function compact<T = any>(arr: T[]): T[];
--- src/utils/commonUtils.ts
+++ src/utils/commonUtils.ts
@@ -84,3 +84,7 @@ export function generateReverseMap(mapObj: Record<string, string>) {
   }
   return ret;
 }
+
+export function compact<T = any>(arr: T[]): T[] {
+  return arr.filter((a) => a !== null && a !== undefined);
+}
