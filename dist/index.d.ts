03c4683c4778ad36f11ac558733070c5e3588476
wanghao <whateverhow@gmail.com>
2022-12-08 11:09:39 -0600

    [master] 	modified:   dist/index.d.ts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -5,5 +5,6 @@ export * from './utils/searchUtils';
 export * from './utils/commonUtils';
 export * from './utils/textUtils';
 export * from './utils/mathUtils';
+export * from './utils/arrayUtils';
 export * from './utils/binaryUtils';
 export { HealthCheck } from './controllers/healthCheck';
--- dist/index.js
+++ dist/index.js
@@ -24,6 +24,7 @@ __exportStar(require("./utils/searchUtils"), exports);
 __exportStar(require("./utils/commonUtils"), exports);
 __exportStar(require("./utils/textUtils"), exports);
 __exportStar(require("./utils/mathUtils"), exports);
+__exportStar(require("./utils/arrayUtils"), exports);
 __exportStar(require("./utils/binaryUtils"), exports);
 var healthCheck_1 = require("./controllers/healthCheck");
 Object.defineProperty(exports, "HealthCheck", { enumerable: true, get: function () { return healthCheck_1.HealthCheck; } });
