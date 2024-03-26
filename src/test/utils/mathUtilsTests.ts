bf0a381d1319191cef3e8f5d039cd82947d1befc
wanghao <whateverhow@gmail.com>
2024-03-25 21:27:29 -0500

    [master]   (use "git restore --staged <file>..." to unstage)

--- src/test/utils/mathUtilsTests.ts
+++ src/test/utils/mathUtilsTests.ts
@@ -1,8 +1,14 @@
 import test from 'ava';
-import { shuffle } from '../../utils/mathUtils';
+import { getSharpeRatio, shuffle } from '../../utils/mathUtils';
 
 test('shuffle', (t) => {
   const arr = [1, 2, 3, 4, 5];
   console.log(`shuffle<number>([1, 2, 3, 4, 5])`, shuffle<number>(arr));
   t.pass();
+});
+
+test('getSharpeRatio', (t) => {
+  const arr = [100, 100.1, 99.9, 100.2, 100.5, 101, 100.8, 102, 103];
+  t.is(getSharpeRatio(arr).sharpeRatio, 13.67038728998157);
+  t.is(getSharpeRatio(arr).dailyProfit, 0.003300291423638413);
 });
